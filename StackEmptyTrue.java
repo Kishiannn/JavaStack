4575ccbd869664e3453a2aef3bfce4d24ee1fb5c
Kishiannn_ <abudachan122@gmail.com>
2025-02-05 22:14:27 +0800

    Create StackEmptyTrue.java

---
+++ StackEmptyTrue.java
@@ -0,0 +1,20 @@
+package com.mycompany.stackjava;
+
+import java.util.Stack;
+
+
+
+public class StackJava {
+
+    public static void main(String[] args) {
+        Stack<String> myHobbies = new Stack <String>();
+        
+        System.out.print(myHobbies.empty());
+    }
+}
+
+
+/*Result
+
+true
+*/
